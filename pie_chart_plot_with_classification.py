708a981fb0fa90ea44dbf916eaf156f584ee2bac
MD UMAR <76789751+MdUmar-tech@users.noreply.github.com>
2024-06-05 15:19:24 +0530

    Create pie_chart_plot_with_classification.py

---
+++ pie_chart_plot_with_classification.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Define categories dictionary
+categories = {
+    'INFORMATION STORAGE AND PROCESSING': ['J', 'A', 'K', 'L', 'B'],
+    'CELLULAR PROCESSES AND SIGNALING': ['D', 'Y', 'V', 'T', 'M', 'N', 'Z', 'W', 'U', 'O'],
+    'METABOLISM': ['C', 'G', 'E', 'F', 'H', 'I', 'P', 'Q'],
+    'POORLY CHARACTERIZED': ['R', 'S', 'X']
+}
+
+# Read the data from the file
+MCCB_246 = pd.read_csv("results/func_stats.txt", header=None, delimiter="\t")
+MCCB_246.drop(index=MCCB_246.index[0], axis=0, inplace=True)
+
+# Rename the columns
+MCCB_246.columns = ["FunctionClass", "Description", "Frequency"]
+
+# Convert 'Frequency' column to numeric
+MCCB_246["Frequency"] = pd.to_numeric(MCCB_246["Frequency"])
+
+# Customize color palette
+palette = sns.color_palette("husl", n_colors=len(MCCB_246["FunctionClass"].unique()))
+
+# Plot the data as a pie chart
+plt.figure(figsize=(14, 14))  # Increase figure size to make the pie chart larger
+plt.pie(MCCB_246["Frequency"], labels=MCCB_246["FunctionClass"], colors=palette, autopct='%1.1f%%', startangle=140, pctdistance=0.9)
+plt.title("COG Function Classification", fontsize=16)
+
+# Create custom legend handles and labels with categories followed by subcategories
+custom_legend_handles = []
+custom_legend_labels = []
+for category, subcategories in categories.items():
+    # Add a custom legend handle and label for the current category
+    custom_legend_handles.append(plt.Line2D([0], [0], color='gray', linestyle='-', linewidth=2))
+    custom_legend_labels.append(category)
+    
+    # Add original legend handles and labels for subcategories of the current category
+    for subcategory in subcategories:
+        if subcategory in MCCB_246["FunctionClass"].values:
+            index = MCCB_246[MCCB_246["FunctionClass"] == subcategory].index[0]
+            label = f"{subcategory}: {MCCB_246.at[index, 'Description']}"
+            custom_legend_handles.append(plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=palette[index % len(palette)], markersize=10))
+            custom_legend_labels.append(label)
+
+# Create the custom legend with smaller size
+plt.legend(custom_legend_handles, custom_legend_labels, bbox_to_anchor=(1, 0.5), loc='center left', title="MMCC30 Function class", prop={'size': 11})  # Smaller size here
+
+# Adjust the layout
+plt.tight_layout(pad=2)
+plt.savefig("pie_chart.png", dpi=300)
+
+plt.show()
